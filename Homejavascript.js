587adf1b324ee5af7dd23d3ed3911fe641a98efd
aref <arefobaid6@gmail.com0>
2022-01-21 23:54:32 +0100

    is finsh

--- Homejavascript.js
+++ Homejavascript.js
@@ -5,4 +5,35 @@ let us = ['usedtools/firma.jpg','usedtools/plakat.jpg']
           console.log(random);
          console.log(Math.random());
         image.src = us[random];
-    }, 5000);   
+    }, 3000); 
+    
+    
+
+    function phon(){
+    let ruf= document.getElementById("nummer");
+    ruf.style.backgroundColor="white";
+    ruf.style.visibility="visible";
+    console.log("aref");
+      setInterval(()=>{
+        ruf.style.visibility="hidden";
+        console.log("aref")
+
+      },5000)
+
+    
+    };
+    function suche(){
+        let ruf= document.getElementById("suche");
+        ruf.style.backgroundColor="white";
+        ruf.style.visibility="visible";
+        console.log("aref");
+          setInterval(()=>{
+            ruf.style.visibility="hidden";
+            console.log("aref")
+    
+          },30000)
+        };
+       function dropDown(){
+        document.getElementById("myDropdown").classList.toggle("show");
+        console.log("aref")
+       }
