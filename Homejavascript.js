a44c5ad9d0260a9a2de7c3af5dfd6a5a2c88c77b
aref <arefobaid6@gmail.com0>
2022-01-23 00:30:06 +0100

    fertig

--- Homejavascript.js
+++ Homejavascript.js
@@ -1,11 +1,4 @@
-let image = document.getElementById('images');
-let us = ['usedtools/firma.jpg','usedtools/plakat.jpg']
-    setInterval(()=>{
-        let random = Math.floor(Math.random() * 2);
-          console.log(random);
-         console.log(Math.random());
-        image.src = us[random];
-    }, 3000); 
+ 
     
     
 
